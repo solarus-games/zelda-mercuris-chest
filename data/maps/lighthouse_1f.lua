2f7daacd55f6234cc34817a46002bfc12a453ecc
is06 <thomas.noury@is06.com>
2013-11-23 17:40:32 +0100

    Torch puzzle in first room of lighthouse

--- data/maps/lighthouse_1f.lua
+++ data/maps/lighthouse_1f.lua
@@ -1,9 +1,16 @@
 local map = ...
 
-function map:on_started()
-  local game = map:get_game()
+local torch_lit_count = 0
+local torch_puzzle_resolved = false
 
+local function locked_door_2_open()
+  map:open_doors("LD2")
+  sol.audio.play_sound("secret")
+end
+
+function map:on_started(destination)
   -- Item initialization (for testing purposes)
+  local game = map:get_game()
   game:get_item("sword"):set_variant(1)
   game:get_item("shield"):set_variant(1)
   game:get_item("lamp"):set_variant(1)
@@ -14,20 +21,27 @@ function map:on_started()
   game:get_item("bombs_counter"):set_variant(1)
   game:get_item("bombs_counter"):set_amount(10)
 
+  -- Open LD2 door if hero comes from B1 level
+  if destination == from_b1_a then
+    map:set_doors_open("LD2", true)
+  end
+
+  -- Shutdown wind in music
+  sol.audio.set_music_channel_volume(6, 0)
+  sol.audio.set_music_channel_volume(7, 0)
+
   -- Torch puzzle
   for torch in map:get_entities("torch_A") do
-    torch.on_activated = torch_A_activated
-  end
-end
-
-function map:on_update()
-  if torch_A1 ~= nil and
-  torch_A1:get_sprite():get_animation() == "lit" and
-  torch_A2:get_sprite():get_animation() == "lit" and
-  torch_A3:get_sprite():get_animation() == "lit" and
-  torch_A4:get_sprite():get_animation() == "lit" then
-    map:remove_entities("torch_A")    
-    sol.audio.play_sound("secret")
-    map:open_doors("torch_door")
+    torch:get_sprite().on_animation_changed = function(sprite, animation)
+      if animation == "lit" then
+        torch_lit_count = torch_lit_count + 1
+      else
+        torch_lit_count = torch_lit_count - 1
+      end
+      if torch_puzzle_resolved == false and torch_lit_count == 4 then
+        map:move_camera(800, 512, 250, locked_door_2_open)
+        torch_puzzle_resolved = true
+      end
+    end
   end
 end
--- data/maps/lighthouse_2f.lua
+++ data/maps/lighthouse_2f.lua
@@ -2,14 +2,22 @@ local map = ...
 
 function map:on_started()
   local platform_movement = sol.movement.create("straight")  
+  platform_movement:set_speed(32)
   platform_movement:set_angle(math.pi)
+  platform_movement:set_ignore_obstacles(true)
+  platform_movement:set_max_distance(48)
   platform_movement:start(moving_platform)
-  
-  moving_platform.on_obstacle_reached = function(movement)
-    if movement:get_angle() == math.pi then
-      movement:set_angle(0)
-    else
-      movement:set_angle(math.pi)
-    end
+
+  platform_movement.on_finished = function()
+    local platform_movement = sol.movement.create("straight")
+    platform_movement:set_speed(32)
+    platform_movement:set_angle(platform_movement:get_angle())
+    platform_movement:set_ignore_obstacles(true)
+    platform_movement:set_max_distance(48)
+    platform_movement:start(moving_platform)
+
+
+    platform_movement:set_angle(platform_movement:get_angle() + math.pi)
+    platform_movement:start(moving_platform)
   end
 end
