65ffd076c288887b3576b6940e4aa9f0b8a0741a
Christopho <christopho@solarus-games.org>
2014-06-14 11:08:25 +0200

    Finish the minecart sprites.

--- data/entities/minecart.lua
+++ data/entities/minecart.lua
@@ -3,3 +3,12 @@
 local minecart = ...
 
 minecart:create_sprite("entities/minecart")
+minecart:set_traversable_by("hero", false)
+
+function minecart:on_interaction()
+
+  hero:freeze()
+  hero:set_animation("minecart_driving")
+  minecart:get_sprite():set_animation("driving")
+end
+
