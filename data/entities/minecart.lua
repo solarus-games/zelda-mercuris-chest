348ab1bfe5fe656a537a05ef1407783b35a4c033
Christopho <christopho@solarus-games.org>
2014-06-18 09:17:11 +0200

    Continuing the minecart.

--- data/entities/minecart.lua
+++ data/entities/minecart.lua
@@ -1,6 +1,9 @@
 -- A minecart to be used by the hero on railroads.
 
 local minecart = ...
+local map = minecart:get_map()
+local game = minecart:get_game()
+local hero = map:get_hero()
 
 minecart:create_sprite("entities/minecart")
 
@@ -19,16 +22,43 @@ end)
 minecart:add_collision_test("containing", function(minecart, other)
 
   if other:get_type() == "custom_entity" then
+
     if other:get_model() == "minecart_turn" then
-      local hero = minecart:get_map():get_hero()
       local movement = hero:get_movement()
       if movement ~= nil then
         -- Simply change the hero's movement direction.
         local direction4 = other:get_direction()
         movement:set_angle(direction4 * math.pi / 2)
+        hero:set_direction(direction4)
       end
     end
+
+    if other:get_model() == "minecart_turn_diagonal" then
+      local movement = hero:get_movement()
+      if movement ~= nil then
+        -- Simply change the hero's movement direction.
+        local direction8 = other:get_direction() * 2 + 1
+        movement:set_angle(direction8 * math.pi / 4)
+        local direction4 = (direction8 == 1 or direction == 7) and 0 or 2
+        hero:set_direction(direction4)
+      end
+    end
+
   end
+end)
+
+-- Show an action icon when the player faces the minecart.
+minecart:add_collision_test("facing", function(minecart, other)
+
+  if other:get_type() == "hero" then
+
+    if minecart:get_movement() == nil
+      and game:get_custom_command_effect("action") == nil then
+      -- TODO also remove the action icon when leaving.
+      -- game:set_custom_command_effect("action", "action")
+    end
+  end
+
 end)
 
 -- Called when the hero presses the action command near the minecart.
@@ -42,7 +72,6 @@ end
 -- Starts driving the minecart.
 function minecart:go()
 
-  local hero = minecart:get_map():get_hero()
   hero:freeze()
   hero:set_position(minecart:get_position())
   hero:set_animation("minecart_driving")
@@ -52,7 +81,7 @@ function minecart:go()
   local direction4 = minecart:get_direction()
   local movement = sol.movement.create("straight")
   movement:set_angle(direction4 * math.pi / 2)
-  movement:set_speed(96)
+  movement:set_speed(128)
 
   function movement:on_position_changed()
     -- Put the minecart at the same position as the hero.
@@ -67,13 +96,32 @@ function minecart:go()
   -- The hero must be allowed to traverse the minecart during the movement.
   minecart:set_traversable_by("hero", true)
 
+  -- Allow the player to control the direction of the hero.
+  if map.on_command_pressed == nil then
+    function map:on_command_pressed(command)
+
+      local directions4 = {
+        right = 0,
+        up = 1,
+        left = 2,
+        down = 3
+      }
+      local direction4 = directions4[command]
+      if direction4 ~= nil then
+        hero:set_direction(direction4)
+        return true  -- Consume the event.
+      end
+
+      return false
+    end
+  end
+
   movement:start(hero)
 end
 
 -- Stops driving the minecart and destroys it.
 function minecart:stop()
 
-  local hero = minecart:get_map():get_hero()
   local minecart_sprite = minecart:get_sprite()
 
   -- Break the minecart.
@@ -85,7 +133,8 @@ function minecart:stop()
     minecart:remove()
   end
 
-  -- Restore control to the player.
+   -- Restore control to the player.
+  map.on_command_pressed = nil
   hero:unfreeze()
 end
 
---
+++ data/entities/minecart_turn_diagonal.lua
@@ -0,0 +1,2 @@
+-- A detector that changes the direction of minecarts, for diagonal directions.
+-- No code here: the minecart script also manages minecart turns.
