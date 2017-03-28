a373d9322ccb242101656f67831413f15bfacba9
Christopho <christopho@solarus-games.org>
2017-03-28 18:29:29 +0200

    Modernize game-over menu code

--- data/scripts/features.lua
+++ data/scripts/features.lua
@@ -8,6 +8,7 @@
 require("scripts/debug")
 require("scripts/equipment")
 require("scripts/dungeons")
+require("scripts/menus/game_over")
 require("scripts/menus/dialog_box")
 require("scripts/menus/pause")
 require("scripts/hud/hud")
