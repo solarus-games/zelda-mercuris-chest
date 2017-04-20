40bf2d5509d3f07e11f542ca6ce9f29f57ff74bb
val <valentin.soudier@gmail.com>
2017-04-21 00:49:08 +0200

    Rename touch_layer.lua

--- data/scripts/features.lua
+++ data/scripts/features.lua
@@ -15,7 +15,7 @@ require("scripts/hud/hud")
 if sol.main.get_os() ~= "iOS" then
   require("scripts/mouse_control")
 else
-  require("scripts/touch_layer")
+  require("scripts/touch_control")
 end
 
 return true
